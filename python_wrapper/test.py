f074c1878a532f9595d45f23405f12981678d334
taotao <335702517@qq.com>
2020-01-04 17:18:27 +0800

    update

--- python_wrapper/test.py
+++ python_wrapper/test.py
@@ -8,6 +8,7 @@ pic_path = '../imgs/720P.jpg'
 decoder = libnvjpeg.py_NVJpegDecoder()
 pic = decoder.imread(pic_path)
 
+print('nvjpeg')
 st = time.time()
 iters = 100
 for i in range(iters):
@@ -16,5 +17,13 @@ st = time.time() - st
 print(st/iters, "sec.")
 print(pic.shape)
 
-cv2.imshow("", pic)
-cv2.waitKey(0)
+#cv2.imshow("", pic)
+#cv2.waitKey(0)
+
+print('opencv')
+st = time.time()
+for i in range(iters):
+    pic = cv2.imread(pic_path)
+st = time.time() - st
+print(st/iters, "sec.")
+
